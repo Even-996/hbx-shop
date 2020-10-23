e263143c48681107a46af45b335347f66e202312
fight-for-fairy <1135421695@qq.com>
2020-10-23 15:17:34 +0800

    更新项目

--- src/views/newest/Newest.vue
+++ src/views/newest/Newest.vue
@@ -11,42 +11,44 @@
         :sm="12"
         :xs="12"
         class="content_goods"
-        v-for="(item, index) in msg"
+        v-for="(item, index) in newcloses"
         :key="index"
       >
         <span
-          v-show="!item.ishover && !item.url2"
+          v-show="!item.isshow && !item.img2"
           class="haveSellAll"
           @mouseenter="imageChange(index)"
-          @mouseleave="imageLeaveChange(index)"
+          @mouseleave="imageLeaveChange(item)"
 		  :md="6"
           :sm="12"
           :xs="12"
           >已售完</span
         >
         <img
-          v-show="item.ishover || !item.url2"
-          :src="item.url1"
-          @mouseenter="imageChange(index)"
-          @mouseleave="imageLeaveChange(index)"
+          v-show="item.isshow || !item.img2"
+          :src="item.img1"
+          @mouseenter="imageChange(item)"
+          @mouseleave="imageLeaveChange(item)"
         />
         <img
-          v-show="!item.ishover && item.url2"
-          :src="item.url2"
-          @mouseenter="imageChange(index)"
-          @mouseleave="imageLeaveChange(index)"
+          v-show="!item.isshow && item.img2"
+          :src="item.img2"
+          @mouseenter="imageChange(item)"
+          @mouseleave="imageLeaveChange(item)"
         /><br />
         <br />
         <div class="title" 
-		@mouseenter="imageChange(index)"
-        @mouseleave="imageLeaveChange(index)"
+		@mouseenter="imageChange(item)"
+        @mouseleave="imageLeaveChange(item)"
 		>
-			<span>{{ item.msg1 }}</span>
-			<router-link to="#">{{ item.msg2 }}</router-link>
-			<router-link to="#" v-if="item.msg3">{{ item.msg3 }}</router-link>
-			<span v-else class="nosell_all">售罄</span>
-			<router-link v-show="!item.ishover" to="#">{{ item.msg4 }}</router-link>
-		</div>
+			<span>{{ item.time }}</span>
+			<router-link to="#">{{ item.name }}</router-link>
+			<span  class="nosell_all" v-if="!item.num">{{ item.state }}</span>
+<!--            v-show="!item.isshow"-->
+			<router-link v-if="item.num" to="#">{{item.state}}</router-link>
+            <router-link to="#" style="position: absolute"  v-if="!item.isshow && item.num">快速查看</router-link>
+
+        </div>
       </el-col>
     </el-row>
     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
@@ -54,63 +56,76 @@
 </template>
 
 <script>
-export default {
+    import {getHomeMultData} from "../../network/home";
+
+    export default {
   name: "Newest",
   data() {
     return {
       msg: [
-        {
-          url1:
-            "https://image-cdn.hypb.st/http%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F9f%2F58%2FShoes_1_0_Drops-18a88146700a02f6b4cc81aa4791.jpg?q=60&w=460&fit=max&v=1",
-          url2:
-            "https://image-cdn.hypb.st/http%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F2a%2Fa5%2FShoes_2_1-98828f66be68923cbc070ba1f451.jpg?q=60&w=460&fit=max&v=1",
-          msg1: "OCT 21, 2020 / 09:00 EST",
-          msg2: "Asics x GmbH Gel-quantum 360 6",
-          msg3: "选购女装",
-          msg4: "快速查看",
-          ishover: true,
-        },
-        {
-          url1:
-            "https://image-cdn.hypb.st/http%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F9f%2F58%2FShoes_1_0_Drops-18a88146700a02f6b4cc81aa4791.jpg?q=60&w=460&fit=max&v=1",
-          url2: "",
-          msg1: "OCT 21, 2020 / 09:00 EST",
-          msg2: "Asics x GmbH Gel-quantum 360 6",
-          msg3: "",
-          msg4: "快速查看",
-          ishover: true,
-        },
-        {
-          url1:
-            "https://image-cdn.hypb.st/http%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F9f%2F58%2FShoes_1_0_Drops-18a88146700a02f6b4cc81aa4791.jpg?q=60&w=460&fit=max&v=1",
-          url2:
-            "https://image-cdn.hypb.st/http%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F2a%2Fa5%2FShoes_2_1-98828f66be68923cbc070ba1f451.jpg?q=60&w=460&fit=max&v=1",
-          msg1: "OCT 21, 2020 / 09:00 EST",
-          msg2: "Asics x GmbH Gel-quantum 360 6",
-          msg3: "选购女装",
-          msg4: "快速查看",
-          ishover: true,
-        },
-        {
-          url1:
-            "https://image-cdn.hypb.st/http%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F9f%2F58%2FShoes_1_0_Drops-18a88146700a02f6b4cc81aa4791.jpg?q=60&w=460&fit=max&v=1",
-          url2: "",
-          msg1: "OCT 21, 2020 / 09:00 EST",
-          msg2: "Asics x GmbH Gel-quantum 360 6",
-          msg3: "",
-          msg4: "快速查看",
-          ishover: true,
-        },
+        // {
+        //   url1:
+        //     "https://image-cdn.hypb.st/http%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F9f%2F58%2FShoes_1_0_Drops-18a88146700a02f6b4cc81aa4791.jpg?q=60&w=460&fit=max&v=1",
+        //   url2:
+        //     "https://image-cdn.hypb.st/http%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F2a%2Fa5%2FShoes_2_1-98828f66be68923cbc070ba1f451.jpg?q=60&w=460&fit=max&v=1",
+        //   msg1: "OCT 21, 2020 / 09:00 EST",
+        //   msg2: "Asics x GmbH Gel-quantum 360 6",
+        //   msg3: "选购女装",
+        //   msg4: "快速查看",
+        //   ishover: true,
+        // },
+        // {
+        //   url1:
+        //     "https://image-cdn.hypb.st/http%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F9f%2F58%2FShoes_1_0_Drops-18a88146700a02f6b4cc81aa4791.jpg?q=60&w=460&fit=max&v=1",
+        //   url2: "",
+        //   msg1: "OCT 21, 2020 / 09:00 EST",
+        //   msg2: "Asics x GmbH Gel-quantum 360 6",
+        //   msg3: "",
+        //   msg4: "快速查看",
+        //   ishover: true,
+        // },
+        // {
+        //   url1:
+        //     "https://image-cdn.hypb.st/http%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F9f%2F58%2FShoes_1_0_Drops-18a88146700a02f6b4cc81aa4791.jpg?q=60&w=460&fit=max&v=1",
+        //   url2:
+        //     "https://image-cdn.hypb.st/http%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F2a%2Fa5%2FShoes_2_1-98828f66be68923cbc070ba1f451.jpg?q=60&w=460&fit=max&v=1",
+        //   msg1: "OCT 21, 2020 / 09:00 EST",
+        //   msg2: "Asics x GmbH Gel-quantum 360 6",
+        //   msg3: "选购女装",
+        //   msg4: "快速查看",
+        //   ishover: true,
+        // },
+        // {
+        //   url1:
+        //     "https://image-cdn.hypb.st/http%3A%2F%2Fs3.store.hypebeast.com%2Fmedia%2Fimage%2F9f%2F58%2FShoes_1_0_Drops-18a88146700a02f6b4cc81aa4791.jpg?q=60&w=460&fit=max&v=1",
+        //   url2: "",
+        //   msg1: "OCT 21, 2020 / 09:00 EST",
+        //   msg2: "Asics x GmbH Gel-quantum 360 6",
+        //   msg3: "",
+        //   msg4: "快速查看",
+        //   ishover: true,
+        // },
       ],
+      newcloses:[]
     };
   },
+        created(){
+            this.loadData();
+        },
   methods: {
-    imageChange(index) {
-      this.msg[index].ishover = false;
+    imageChange(item) {
+       item.isshow = 0;
     },
-    imageLeaveChange(index) {
-      this.msg[index].ishover = true;
+    imageLeaveChange(item) {
+        item.isshow = 1;
     },
+      loadData(){
+        getHomeMultData().then(res=>{
+            this.newcloses = res.data.newcloses;
+
+        }).catch(error=>{
+        })
+      }
   },
 };
 </script>
@@ -132,6 +147,7 @@ export default {
 }
 .content_goods {
   text-align: center;
+    padding-bottom: .5rem;
 }
 .content_goods a {
   display: block;
